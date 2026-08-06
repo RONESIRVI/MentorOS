89db85bafe4835004085e47a57dc90a94c91328d
RONESIRVI <figuring.cse@gmail.com>
2026-08-06 15:45:38 +0530

    Update snippet_categories.json via bat file

---
+++ snippet_categories.js
@@ -0,0 +1,25 @@
+window.SNIPPET_CATEGORIES = {
+  "categories": [
+    "Ethics",
+    "Thinkers",
+    "Gov",
+    "👥Society",
+    "💼Public Service"
+  ],
+  "ranks": [
+    "Rank 1 - Shruti Sharma"
+  ],
+  "years": [
+    "2023",
+    "2024",
+    "2025"
+  ],
+  "papers": [
+    "GS Paper 1",
+    "GS Paper 2",
+    "GS Paper 3",
+    "Ethics",
+    "Essay",
+    "PUB ADD"
+  ]
+};
